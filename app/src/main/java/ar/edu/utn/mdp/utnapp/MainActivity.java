660217b34668291774924e4fff0883645c12f496
Matias Aceval <matiasacevalventura@gmail.com>
2022-05-22 19:28:40 -0300

    🆕 initial commit

---
+++ app/src/main/java/ar/edu/utn/mdp/utnapp/MainActivity.java
@@ -0,0 +1,16 @@
+package ar.edu.utn.mdp.utnapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_main);
+    }
+}
