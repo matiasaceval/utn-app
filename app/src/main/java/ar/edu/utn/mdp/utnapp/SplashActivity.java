2c5b9ac1a30aeb9f3e050544d9dee0bff05319e0
Matias Aceval <matiasacevalventura@gmail.com>
2022-05-31 18:52:07 -0300

    🎨 refactor: white theme replacing with black one

--- app/src/main/java/ar/edu/utn/mdp/utnapp/SplashActivity.java
+++ app/src/main/java/ar/edu/utn/mdp/utnapp/SplashActivity.java
@@ -14,6 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
 
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(SplashActivity.this, MainActivity.class);
